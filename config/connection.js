1b13a253737c2a12bb50b73bbb9655664a9e421b
Sara Nolan <saraa.ann28@gmail.com>
2021-05-11 17:03:03 -0400

    changes pw in connection.js

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "rootpw",
+    password: process.env.DB_PASSWORD,
     database: "burgers_db"
   });
 };
