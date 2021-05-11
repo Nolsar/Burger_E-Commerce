cefcb4ef4ea75ce5da81a28f19bf0b4d710639d5
Sara Nolan <saraa.ann28@gmail.com>
2021-05-11 17:12:59 -0400

    edited conenction.js

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,24 @@
-var mysql = require("mysql");
-var connection; 
+// Set up MySQL connection.
+const mysql = require("mysql");
 
-if (process.env.JAWSDB_URL) {
+
+let connection;
+if(process.env.JAWSDB_URL && process.env.JAWSDB_URL.length > 0){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
+}
+else{
   connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: process.env.DB_PASSWORD,
+    password: "rootpw",
     database: "burgers_db"
   });
-};
+}
 
+
+
+// Make connection.
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -21,4 +27,35 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
+// turn on logging
+connection.on('enqueue', function(query) { console.log(query.sql) });
+
+// Export connection for our ORM to use.
 module.exports = connection;
+
+
+// var mysql = require("mysql");
+// var connection; 
+
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//   connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "rootpw",
+//     database: "burgers_db"
+//   });
+// };
+
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+// module.exports = connection;
+
